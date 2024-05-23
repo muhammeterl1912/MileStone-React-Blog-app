834b5ded8c909c3ee3aa875ef1024eeaaa115b1c
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-05-23 14:16:12 +0200

    Added loading gif to Detail compenent.

--- src/components/pages/Detail.jsx
+++ src/components/pages/Detail.jsx
@@ -9,16 +9,25 @@ import VisibilityIcon from "@mui/icons-material/Visibility";
 import Avatar from "@mui/material/Avatar";
 import { getBlogStateDetail, postBlogLike } from "../services/BlogCalls";
 import CommentCard from "../blog/CommentCard";
+import LoadingGif from "../../assets/Loading.gif";
 
 const Detail = () => {
   const { id } = useParams();
   const [showComment, setShowComment] = useState(false);
   const dispatch = useDispatch();
-  const { singleBlog, isLiked } = useSelector((state) => state.blogs);
+  const { singleBlog, isLiked, loading } = useSelector((state) => state.blogs);
 
   useEffect(() => {
     getPostDetail();
   }, [id, isLiked]);
+
+  const getPostDetail = () => {
+    dispatch(getBlogStateDetail(id));
+  };
+
+  const handleClickLike = (id) => {
+    dispatch(postBlogLike(id));
+  };
 
   const sampleBlog = {
     id: singleBlog?._id,
@@ -30,130 +39,130 @@ const Detail = () => {
     comments: singleBlog?.comments || [],
     likes: singleBlog?.likes || [],
     countOfVisitors: singleBlog?.countOfVisitors || [],
-    createdAt: singleBlog
-      ? new Date(singleBlog.createdAt).toLocaleString()
-      : "",
-    updatedAt: singleBlog
-      ? new Date(singleBlog.updatedAt).toLocaleString()
-      : "",
-    firstName: singleBlog?.userId ? singleBlog?.userId.firstName : "Unknown",
-    lastName: singleBlog?.userId ? singleBlog?.userId.lastName : "Unknown",
-  };
-
-  const handleClickLike = (id) => {
-    dispatch(postBlogLike(id));
-  };
-
-  const getPostDetail = () => {
-    dispatch(getBlogStateDetail(id));
+    createdAt: singleBlog ? new Date(singleBlog.createdAt).toLocaleString() : "",
+    updatedAt: singleBlog ? new Date(singleBlog.updatedAt).toLocaleString() : "",
+    firstName: singleBlog?.userId ? singleBlog.userId.firstName : "Unknown",
+    lastName: singleBlog?.userId ? singleBlog.userId.lastName : "Unknown",
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "50px",
-        alignItems: "center",
-        minHeight: "80vh",
-        marginBottom: "100px",
-        background: "#f9f9f9",
-      }}
-    >
+    loading ? (
       <div
         style={{
-          width: "100%",
-          maxWidth: 800,
-          margin: "20px",
-          padding: "40px",
-          borderRadius: "8px",
-          boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-          background: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+        }}
+      >
+        <img src={LoadingGif} alt="Loading..." style={{ width: "100px", height: "100px" }} />
+      </div>
+    ) : (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          padding: "50px",
+          alignItems: "center",
+          minHeight: "80vh",
+          marginBottom: "100px",
+          background: "#f9f9f9",
         }}
       >
         <div
           style={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: "20px",
-          }}
-        >
-          <Avatar
-            alt="Author Avatar"
-            src={""}
-            style={{ marginRight: "10px" }}
-          />
-          <Typography variant="h6" gutterBottom style={{ fontWeight: "bold" }}>
-            {sampleBlog.title}
-          </Typography>
-        </div>
-        <img
-          src={sampleBlog.image}
-          alt="Blog image"
-          style={{
-            height: 300,
             width: "100%",
-            objectFit: "cover",
-            borderRadius: "8px 8px 0 0",
-            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
-          }}
-        />
-        <div style={{ marginTop: "20px" }}>
-          <Typography variant="body1" color="textSecondary">
-            {sampleBlog.content}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            style={{ marginTop: "10px", fontStyle: "italic" }}
-          >
-            Publish Date: {sampleBlog.createdAt}
-          </Typography>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            style={{ marginTop: "10px", fontStyle: "italic" }}
-          >
-            Author: {`${sampleBlog.firstName} ${sampleBlog.lastName}`}
-          </Typography>
-        </div>
-        <div
-          style={{
-            marginTop: "20px",
-            display: "flex",
-            justifyContent: "space-between",
+            maxWidth: 800,
+            margin: "20px",
+            padding: "40px",
+            borderRadius: "8px",
+            boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+            background: "white",
           }}
         >
-          <div>
-            <IconButton
-              aria-label="add to favorites"
-              onClick={() => handleClickLike(sampleBlog.id)}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "20px",
+            }}
+          >
+            <Avatar
+              alt="Author Avatar"
+              src={sampleBlog.userId?.avatar || ""}
+              style={{ marginRight: "10px" }}
+            />
+            <Typography variant="h6" gutterBottom style={{ fontWeight: "bold" }}>
+              {sampleBlog.title}
+            </Typography>
+          </div>
+          <img
+            src={sampleBlog.image}
+            alt="Blog image"
+            style={{
+              height: 300,
+              width: "100%",
+              objectFit: "cover",
+              borderRadius: "8px 8px 0 0",
+              boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+            }}
+          />
+          <div style={{ marginTop: "20px" }}>
+            <Typography variant="body1" color="textSecondary">
+              {sampleBlog.content}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              style={{ marginTop: "10px", fontStyle: "italic" }}
             >
-              {sampleBlog.likes.length}{" "}
-              <FavoriteIcon
-             sx={{
-  color: isLiked?.didUserLike ? "red" : "black"
-}}
-
-              />
-            </IconButton>
-
-            <IconButton
-              aria-label="comment"
-              onClick={() => {
-                setShowComment((comment) => !comment);
-              }}
+              Publish Date: {sampleBlog.createdAt}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              style={{ marginTop: "10px", fontStyle: "italic" }}
             >
-              {sampleBlog.comments.length} <InsertCommentOutlinedIcon />
+              Author: {`${sampleBlog.firstName} ${sampleBlog.lastName}`}
+            </Typography>
+          </div>
+          <div
+            style={{
+              marginTop: "20px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>
+              <IconButton
+                aria-label="add to favorites"
+                onClick={() => handleClickLike(sampleBlog.id)}
+              >
+                {sampleBlog.likes.length}{" "}
+                <FavoriteIcon
+                  sx={{
+                    color: isLiked?.didUserLike ? "red" : "black",
+                  }}
+                />
+              </IconButton>
+
+              <IconButton
+                aria-label="comment"
+                onClick={() => {
+                  setShowComment((prev) => !prev);
+                }}
+              >
+                {sampleBlog.comments.length} <InsertCommentOutlinedIcon />
+              </IconButton>
+            </div>
+            <IconButton aria-label="view">
+              {sampleBlog.countOfVisitors} <VisibilityIcon />
             </IconButton>
           </div>
-          <IconButton aria-label="view">
-            {sampleBlog.countOfVisitors} <VisibilityIcon />
-          </IconButton>
+          {showComment && <CommentCard />}
         </div>
-        {showComment && <CommentCard />}
       </div>
-    </div>
+    )
   );
 };
 
